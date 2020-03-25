76154d07b6a7cea61779faed6ace8f0e3864236a
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-03-24 22:09:24 -0400

    rust mandel start

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,43 +1,118 @@
+use derive_more::Add;
 use rand::Rng;
 use rayon::prelude::*;
+use std::ops::Mul;
 use std::time::Instant;
 
-fn main() {
-    let init_time = Instant::now();
-    let arr = randoms(5000000);
-    println!(
-        "init     took {:} \tmilliseconds",
-        init_time.elapsed().as_millis()
-    );
-    let serial_time = Instant::now();
-    let s = sum_of_squares_serial(&arr);
-    println!(
-        "serial   took {:} \tmilliseconds",
-        serial_time.elapsed().as_millis()
-    );
-    let parallel_time = Instant::now();
-    let p = sum_of_squares_parallel(&arr);
-    println!(
-        "parallel took {:} \tmilliseconds",
-        parallel_time.elapsed().as_millis()
-    );
-    assert!(s == p);
+#[derive(Debug, Add, Copy, Clone)]
+struct Complex {
+    re: f64,
+    im: f64,
 }
 
-fn randoms(howmany: i32) -> Vec<i64> {
-    (0..howmany)
+impl Mul for Complex {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self {
+        Complex {
+            re: self.re * rhs.re - self.im * rhs.im,
+            im: self.re * rhs.im + self.im * rhs.re,
+        }
+    }
+}
+
+impl Complex {
+    fn sqr_magnitude(self) -> f64 {
+        self.re * self.re + self.im * self.im
+    }
+    fn magnitude(self) -> f64 {
+        self.sqr_magnitude().sqrt()
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Pixel {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug)]
+struct MandelbrotInput {
+    pix: Pixel,
+    c: Complex,
+}
+
+#[derive(Debug)]
+struct MandelbrotOutput {
+    pix: Pixel,
+    iterations: i32,
+}
+
+fn main() {
+    let compute_time = Instant::now();
+    let arr = mandelbrot(
+        500,
+        Complex {
+            re: -0.25,
+            im: 0.65,
+        },
+        0.025,
+        1000,
+    );
+    println!(
+        "computation done in {:.3} seconds",
+        compute_time.elapsed().as_secs_f64()
+    );
+}
+
+fn mandelbrot(
+    resolution: i32,
+    center: Complex,
+    width: f64,
+    max_iters: i32,
+) -> Vec<MandelbrotOutput> {
+    let points = mandelbrot_points(resolution, center, width);
+    points
+        .par_iter()
+        .map(|p| mandelbrot_iterate(p, max_iters))
+        .collect()
+}
+
+fn mandelbrot_points(resolution: i32, center: Complex, width: f64) -> Vec<MandelbrotInput> {
+    let half = width / 2.0;
+    let step = width / (resolution as f64);
+    (0..resolution * resolution)
         .into_par_iter()
-        .map(|_| {
-            let mut rng = rand::thread_rng();
-            rng.gen_range(1, 21)
+        .map(|ind| {
+            let x = ind / resolution;
+            let y = ind % resolution;
+            MandelbrotInput {
+                pix: Pixel { x: x, y: y },
+                c: Complex {
+                    re: x as f64 * step + center.re - half,
+                    im: y as f64 * step + center.im - half,
+                },
+            }
         })
         .collect()
 }
 
-fn sum_of_squares_parallel(input: &[i64]) -> i64 {
-    input.par_iter().map(|&i| i * i).sum()
+fn mandelbrot_iterate(p: &MandelbrotInput, max_iters: i32) -> MandelbrotOutput {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..max_iters {
+        if z.sqr_magnitude() >= 4.0 {
+            return MandelbrotOutput {
+                pix: p.pix.clone(),
+                iterations: i,
+            };
+        }
+        z = mandelbrot_step(z, p.c)
+    }
+    return MandelbrotOutput {
+        pix: p.pix.clone(),
+        iterations: max_iters,
+    };
 }
 
-fn sum_of_squares_serial(input: &[i64]) -> i64 {
-    input.iter().map(|&i| i * i).sum()
+fn mandelbrot_step(z: Complex, c: Complex) -> Complex {
+    z * z + c
 }
